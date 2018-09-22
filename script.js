962fcbd9c03fcdb3a877058a7ebc1fecefa13620
Lkabeer <ahmed@Lkabeer.com>
2018-09-22 09:53:34 +0200

    version X2.5

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 $('div').on('click', function() {
     $(this).toggleClass('show');
 });
+
+window.onscroll = function() {
+    displayTopX();
+}
+
+function displayTopX() {
+    if (document.body.scrollTop > 140 || document.documentElement.scrollTop > 140) {
+        document.getElementById('topX').style.display = "block";
+    } else {
+        document.getElementById('topX').style.display = "none";
+    }
+}
+
+function goTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
