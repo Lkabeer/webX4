54f98caa40bc2367ff99c77f1016d5abd1e206cd
Lkabeer <ahmed@Lkabeer.com>
2018-09-26 14:12:51 +0200

    version X3.0

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
-$('div').on('click', function() {
-    $(this).toggleClass('show');
-});
+var topX = document.getElementById('topX');
+var burgerMenuX = document.getElementById('burgerMenuX');
+var closeX = document.getElementById('closeX');
+var sideMenuX = document.getElementById('sideMenuX');
+
+sideMenuX.style.width = 0;
 
 window.onscroll = function() {
     displayTopX();
 }
 
 function displayTopX() {
-    if (document.body.scrollTop > 140 || document.documentElement.scrollTop > 140) {
-        document.getElementById('topX').style.display = "block";
+    if (document.body.scrollTop > 190 || document.documentElement.scrollTop > 190) {
+        topX.style.display = "block";
+        burgerMenuX.style.display = "block";
     } else {
-        document.getElementById('topX').style.display = "none";
+        topX.style.display = "none";
+        burgerMenuX.style.display = "none";
     }
 }
 
 function goTop() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+function closeMenu(thisX) {
+    thisX.classList.toggle('closeX');
+    if(sideMenuX.style.width == (0 || "0px")) {
+        sideMenuX.style.width = "250px";
+    } else {
+        sideMenuX.style.width = 0;
+    }
 }
